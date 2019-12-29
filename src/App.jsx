053e81c07cb451ae1fd1dda5930a5183a8d8ea15
Ryan <ryan@dksit.com>
2019-12-28 17:07:05 -0700

    created contact view

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Work from "./Views/Work/Work";
 import Demos from "./Views/Demos/Demos";
 import Testimonial from "./Views/Testimonial/Testimonial";
 import About from "./Views/About/About";
+import Contact from "./Views/Contact/Contact";
 
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
         <Testimonial />
         <Work />
         <About />
+        <Contact />
         
            
             
---
+++ src/Views/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import voContact from "../../images/Contact2.png"
+import './Contact.css'
+export class Contact extends Component {
+    render() {
+        return (
+            <div className="Contact-wrapper">
+                <section id="contact">
+                    <h1>Contact</h1>
+                    <img src={voContact} className="contact-img" alt="work title bar" />
+                        </section>
+                    <div className="Contact-email">
+
+                    <a
+                  className="Demos-email"
+                  href="mailto:mike@mikewadevo.com">mike@mikewadevo.com
+                  </a>
+                  </div>
+                    
+            </div>
+        )
+    }
+}
+
+export default Contact
