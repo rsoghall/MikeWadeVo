62ba21b33f5789bb7611b7d9a4a40ca111685558
rsoghall <ryan@dksit.com>
2020-01-23 20:40:15 -0700

    Added mp3 links to Books

--- src/Views/Work/Work.jsx
+++ src/Views/Work/Work.jsx
@@ -5,10 +5,62 @@ import bookCant from "../../images/bookCantSell.jpg"
 import bookEnrollment from "../../images/twEnrollment.png"
 import './Work.css'
 import VideoPlayer from '../../Components/VideoPlayer'
+import ReactModal from "react-modal";
+import Play from "../../images/Play11.png";
+import CantSell from '../../audio/Cant_Sell.mp3'
+import DefFor from '../../audio/Defend_Foreclosure.mp3'
+import EnEff from '../../audio/Enrollment_Effect.mp3'
+
+const customStyles = {
+    content: {
+      top: "31%",
+      left: "15%",
+      position: "absolute",
+      bottom: "auto",
+      padding: "0",
+      width: "12rem",
+      transform: "translate(-50%, -50%)",
+      border: "none",
+      borderRadius: "50px",
+      background: "none",
+      },
+    overlay: {
+      background: "none"
+    }
+  };
 
 
 export class Work extends Component {
+    constructor() {
+        super();
+        this.state = {
+          modalIsOpen: false,
+          audioSource: ""
+        };
+    }
+
+    openModal = audioSource => {
+        this.setState({ modalIsOpen: true, audioSource });
+      };
+    
+      afterOpenModal() {
+        this.subtitle.style.color = "#f00";
+      }
+    
+      closeModal = () => {
+        this.setState({ modalIsOpen: false, audioSource: "" });
+      };
+    
     render() {
+        let audioSource = this.state.audioSource
+    if (audioSource === 'CantSell') {
+      audioSource = CantSell
+    } else if (audioSource === 'DefFor') {
+      audioSource = DefFor
+    } else if (audioSource === 'EnEff') {
+      audioSource = EnEff
+    }
+           
         return (
             <div className="work-wrapper">
                 <section id="work">
@@ -18,16 +70,64 @@ export class Work extends Component {
                 <div className="work-displayWrapper">
 
                 <div className="work-images">
-                    <div>
-                        <img src={bookForeclosure} className="work-image1" alt="Foreclosure Book"/>
+                        <div className = "work-bookplay">  
+                            <img
+                                src={bookForeclosure}
+                                className="work-image1"
+                                alt="Foreclosure Book"
+                            />
+                            
+                            <img
+                                src={Play}
+                                className="work-play"
+                                alt="Play Button"
+                                onClick={() => this.openModal("DefFor")}
+                            />
                     </div>
-                    <div>
-                        <img src={bookCant} className="work-image2" alt="Foreclosure Book" />
+                    <div className = "work-bookplay">
+                            <img
+                                src={bookCant}
+                                className="work-image2"
+                                alt="Can't Sell Book"
+                            />
+
+                            <img
+                                src={Play}
+                                className="work-play"
+                                alt="Play Button"
+                                onClick={() => this.openModal("CantSell")}
+                            />
                     </div>
-                    <div>
-                        <img src={bookEnrollment} className="work-image3" alt="Foreclosure Book"/>
+                    <div className = "work-bookplay">
+                            <img
+                                src={bookEnrollment}
+                                className="work-image3"
+                                alt="Foreclosure Book"
+                            />
+                            <img
+                                src={Play}
+                                className="work-play"
+                                alt="Play Button"
+                                onClick={() => this.openModal("EnEff")}
+                            />
                     </div>
-                </div>
+                    </div>
+                    
+                    <ReactModal
+            style={customStyles}
+            isOpen={this.state.modalIsOpen}
+            onRequestClose={this.closeModal}
+          >
+            <audio
+              controls
+              autoPlay
+              controlsList="nodownload"
+              style={{ opacity: "0.5" }}
+            >
+              <source src={audioSource} type="audio/mpeg" />
+            </audio>
+                    </ReactModal>
+                    
                 <div className="work-video">
 
                 <VideoPlayer />
